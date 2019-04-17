07d2d9d886a6d0fb7d625f1dd304324589a61ce0
Jean Ha <jeanha124@gmail.com>
2019-04-17 03:57:43 -0400

    "Create comments controller to test"

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  before_action :require_login!
+
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      @photo = Photo.find(@comment.photo_id)
+      @comments = @photo.comments
+      render :show
+    else
+      render :create
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render :show
+    else
+      render :show
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :commenter_id, :photo_id)
+  end
+  
+end
